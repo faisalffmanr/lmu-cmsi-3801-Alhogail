3f8d4683f9e77579d61639bd1583c9d3eb2abd31
Faisal Alhogail <faisalalnassraj@gmail.com>
2024-12-06 17:37:48 -0800

    fixed the  deadlock

--- go/restaurant.go
+++ go/restaurant.go
@@ -8,10 +8,11 @@ import (
 	"time"
 )
 
+// Order struct 
 type Order struct {
-	id        uint64
-	customer  string
-	replyChan chan *Order
+	id         uint64
+	customer   string
+	replyChan  chan *Order
 	preparedBy string
 }
 
@@ -21,26 +22,23 @@ func do(seconds int, action ...any) {
 	time.Sleep(time.Duration(randomMillis) * time.Millisecond)
 }
 
-
 var waiter = make(chan *Order, 3)
-
 
 var orderIDCounter atomic.Uint64
 
-
-func cook(name string, wg *sync.WaitGroup) {
-	defer wg.Done()
+func cook(name string, cookWg *sync.WaitGroup) {
+	defer cookWg.Done()
 	log.Println(name, "starting work")
-	for order := range waiter {
+	for order := range waiter { 
 		do(10, name, "cooking order", order.id, "for", order.customer)
 		order.preparedBy = name
-		order.replyChan <- order
+		order.replyChan <- order 
 	}
+	log.Println(name, "shutting down")
 }
 
-
-func customer(name string, wg *sync.WaitGroup) {
-	defer wg.Done()
+func customer(name string, customerWg *sync.WaitGroup) {
+	defer customerWg.Done()
 	log.Println(name, "starting dining experience")
 	mealsEaten := 0
 	for mealsEaten < 5 {
@@ -50,6 +48,7 @@ func customer(name string, wg *sync.WaitGroup) {
 			replyChan: make(chan *Order, 1),
 		}
 		log.Println(name, "placed order", order.id)
+
 		select {
 		case waiter <- order: 
 			meal := <-order.replyChan 
@@ -58,36 +57,40 @@ func customer(name string, wg *sync.WaitGroup) {
 		case <-time.After(7 * time.Second): 
 			do(5, name, "waiting too long, abandoning order", order.id)
 		}
+
 		
 		time.Sleep(time.Duration(1000+rand.Intn(1000)) * time.Millisecond)
 	}
 	log.Println(name, "going home after eating", mealsEaten, "meals")
 }
 
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	log.SetFlags(log.LstdFlags)
 
-	var wg sync.WaitGroup
+	var customerWg sync.WaitGroup 
+	var cookWg sync.WaitGroup     
 
 	
-	cooks := []string{"Ramy", "David", "Linguini"}
+	cooks := []string{"Remy", "Colette", "Linguini"}
 	for _, cookName := range cooks {
-		wg.Add(1)
-		go cook(cookName, &wg)
+		cookWg.Add(1)
+		go cook(cookName, &cookWg)
 	}
 
 	
 	customers := []string{"Faisal", "Fahad", "Khaled", "Yazeed", "Sama", "Sara", "Jawad", "Meshary", "Nawaf", "Turki"}
 	for _, customerName := range customers {
-		wg.Add(1)
-		go customer(customerName, &wg)
+		customerWg.Add(1)
+		go customer(customerName, &customerWg)
 	}
 
-
-	wg.Wait()
-
+	
+	customerWg.Wait()
+	
 	close(waiter)
 
+	cookWg.Wait()
 	log.Println("The restaurant is closing")
 }
